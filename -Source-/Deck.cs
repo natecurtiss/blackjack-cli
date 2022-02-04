14303ee051d4be242e6e1f852c168bb61720e6aa
N8Dev <n8dev.cs@gmail.com>
2022-02-04 18:36:02 -0500

    Add cards and deck

---
+++ -Source-/Deck.cs
@@ -0,0 +1,29 @@
+﻿using static Blackjack.Cards;
+
+namespace Blackjack;
+
+public sealed class Deck
+{
+    readonly List<int> _cards = new();
+
+    public Deck()
+    {
+        var suit = new[]
+        {
+            TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, 
+            JACK, QUEEN, KING, ACE
+        };
+        foreach (var card in suit)
+            for (var i = 0; i < 4; i++)
+                _cards.Add(card);
+    }
+
+    public void Shuffle() => _cards.Shuffle();
+
+    public int Take()
+    {
+        var card = _cards[0];
+        _cards.RemoveAt(0);
+        return card;
+    }
+}
---
+++ -Source-/ListExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Blackjack;
+
+static class ListExtensions
+{
+    static readonly Random _random = new();
+    
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        var i = list.Count;
+        while (i > 1)
+        {
+            i--;
+            var k = _random.Next(i + 1);
+            (list[k], list[i]) = (list[i], list[k]);
+        }
+    }
+}
